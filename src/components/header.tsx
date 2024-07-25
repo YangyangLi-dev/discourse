c80ca48ba97efe6e3e5de0e311ea6d6dcabd0534
YangyangLi-can <yangyangli.ca@gmail.com>
2024-07-25 11:08:18 -0400

    init project

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { Suspense } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react';
+import HeaderAuth from '@/components/header-auth';
+import SearchInput from '@/components/search-input';
+
+export default function Header() {
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Suspense>
+            <SearchInput />
+          </Suspense>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  );
+}
